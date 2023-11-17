11cf78006c4a5d5795308c492af7049c727729d4
Goutham Susarla <34808867+Gouthamsusarla@users.noreply.github.com>
2023-11-17 14:32:26 -0500

    Changes made to metrics.py
    Created metrics and GUI classes and added some changes in the GUI

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -64,20 +64,21 @@ class MetricsGUI:
         self.root.title("Metrics Calculator")
         self.metrics = None
         self.generated_data = []
+        self.valid_input = True  # Flag to check if the input is valid
 
         # Create and set up input entry
-        self.input_label = tk.Label(root, text="Enter a number greater than or equal to 1000:")
-        self.input_label.grid(row=0, column=0, columnspan=2, pady=5)
-        self.input_entry = tk.Entry(root)
-        self.input_entry.grid(row=1, column=0, columnspan=2, pady=5)
+        self.input_label = tk.Label(root, text="Enter a number greater than or equal to 1000:", font=("Helvetica", 12))
+        self.input_label.grid(row=0, column=0, columnspan=2, pady=5, padx=10)
+        self.input_entry = tk.Entry(root, font=("Helvetica", 12))
+        self.input_entry.grid(row=1, column=0, columnspan=2, pady=5, padx=10, ipady=5, sticky="ew")
         self.input_entry.bind("<Return>", lambda event: self.generate_data())
 
         # Create and set up button for generating random data
-        self.generate_button = tk.Button(root, text="Generate Data", command=self.generate_data)
-        self.generate_button.grid(row=2, column=0, columnspan=2, pady=10)
+        self.generate_button = tk.Button(root, text="Generate Data", command=self.generate_data, font=("Helvetica", 12))
+        self.generate_button.grid(row=2, column=0, columnspan=2, pady=10, padx=10, ipady=5, sticky="ew")
 
         # Create and set up listbox for displaying generated data
-        self.data_listbox = tk.Listbox(root, height=10, width=40, selectmode=tk.MULTIPLE)
+        self.data_listbox = tk.Listbox(root, height=10, width=40, selectmode=tk.MULTIPLE, font=("Courier", 10))
         self.data_listbox.grid(row=0, rowspan=3, column=2, pady=10, padx=10, sticky="ns")
 
         # Create scrollbar for data_listbox
@@ -86,18 +87,20 @@ class MetricsGUI:
         self.data_listbox.config(yscrollcommand=self.scrollbar.set)
 
         # Create and set up button for copying data
-        self.copy_button = tk.Button(root, text="Copy Data", command=self.copy_data)
-        self.copy_button.grid(row=3, column=2, columnspan=2, pady=5)
+        self.copy_button = tk.Button(root, text="Copy Data", command=self.copy_data, font=("Helvetica", 12))
+        self.copy_button.grid(row=3, column=2, columnspan=2, pady=5, padx=10, ipady=5, sticky="ew")
 
         # Create and set up result box
-        self.result_box = tk.Text(root, height=10, width=30)
-        self.result_box.grid(row=4, column=0, columnspan=4, pady=10)
+        self.result_box = tk.Text(root, height=7, width=30, font=("Courier", 10))
+        self.result_box.grid(row=4, column=0, columnspan=4, pady=5, padx=10, sticky="ns")
 
         # Create and set up buttons for each metric
         metrics = ["Mean", "Median", "Mode", "Mean Absolute Deviation", "Standard Deviation", "Min", "Max"]
         for i, metric in enumerate(metrics):
-            button = ttk.Button(root, text=metric, command=lambda m=metric: self.calculate_and_display(m))
-            button.grid(row=i + 5, column=0, columnspan=4, pady=5)
+            button = ttk.Button(root, text=metric, command=lambda m=metric: self.calculate_and_display(m),
+                                style="Metrics.TButton")
+            column = i % 2
+            button.grid(row=i // 2 + 5, column=column * 2, pady=5, padx=10, ipadx=10, ipady=5, sticky="ew")
 
     def generate_data(self):
         try:
@@ -109,18 +112,24 @@ class MetricsGUI:
             self.generated_data.append(data)
             self.update_data_listbox(data)
             self.reset_result_box()
+            self.valid_input = True  # Set input flag to True for valid input
         except ValueError as e:
+            self.data_listbox.delete(0, tk.END)  # Clear existing items in the list box
             self.result_box.delete(1.0, tk.END)  # Clear existing results
             self.result_box.insert(tk.END, str(e))
+            self.valid_input = False  # Set input flag to False for invalid input
 
     def calculate_and_display(self, metric):
         try:
-            if self.metrics is not None:  # Check if data has been generated
+            if self.metrics is not None and self.valid_input:  # Check if data has been generated and input is valid
                 # Convert metric label to snake_case
                 metric_method = metric.lower().replace(" ", "_")
                 result = getattr(self.metrics, metric_method)()
                 self.result_box.delete(1.0, tk.END)  # Clear existing results
                 self.result_box.insert(tk.END, f"{metric}: {result}")
+            elif not self.valid_input:
+                self.result_box.delete(1.0, tk.END)  # Clear existing results
+                self.result_box.insert(tk.END, "Please generate valid data first.")
             else:
                 self.result_box.delete(1.0, tk.END)  # Clear existing results
                 self.result_box.insert(tk.END, "Please generate data first.")
@@ -132,7 +141,7 @@ class MetricsGUI:
         self.data_listbox.delete(0, tk.END)  # Clear existing items
         formatted_data = ", ".join(map(str, data))
         split_data = formatted_data.split(", ")
-        lines = [", ".join(split_data[i:i+10]) for i in range(0, len(split_data), 10)]
+        lines = [", ".join(split_data[i:i + 10]) for i in range(0, len(split_data), 10)]
         for i, line in enumerate(lines):
             if i < len(lines) - 1:
                 self.data_listbox.insert(tk.END, line + ", ")
